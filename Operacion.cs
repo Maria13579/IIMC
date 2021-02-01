d37924b1ef5779b14a4cd2c12c1d9becd5972a01
María Patricia Serrano Ramírez <201800378@colsanjose.edu.gt>
2021-02-01 17:19:49 -0600

    Creación del Proyecto

---
+++ Operacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IIMC
+{
+    class Operacion
+    {
+        public Double imc (double a, double b)
+        {
+            return (a / (b * b));
+        }
+    }
+}
